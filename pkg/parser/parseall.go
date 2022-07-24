18aa3d4ecd787c122b77087674a701dfa5c1b517
James Jarvis <git@jamesjarvis.io>
2022-07-24 09:37:51 +0100

    make parsing concurrent to speed up execution, needs upstream fix from massivelyconcurrentsystems

--- pkg/parser/parseall.go
+++ pkg/parser/parseall.go
@@ -1,24 +1,77 @@
 package parser
 
 import (
+	"context"
 	"fmt"
+	"runtime"
+	"sync"
 
 	"log"
 
 	"github.com/jamesjarvis/mappyboi/pkg/models"
+	pool "github.com/jamesjarvis/massivelyconcurrentsystems/pool"
 )
+
+type parserWork struct {
+	parser Parser
+	wg     *sync.WaitGroup
+
+	Error error
+	Data  *models.Data
+}
 
 func ParseAll(parsers ...Parser) (*models.Data, error) {
 	data := &models.Data{}
-	var tempData *models.Data
-	var err error
-	for _, p := range parsers {
-		tempData, err = p.Parse()
+
+	workerFunc := func(p *parserWork) error {
+		defer p.wg.Done()
+		tempData, err := p.parser.Parse()
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse '%s': %w", p.String(), err)
+			p.Error = fmt.Errorf("failed to parse '%s': %w", p.parser.String(), err)
+			return p.Error
 		}
-		data.GoLocations = append(data.GoLocations, tempData.GoLocations...)
-		log.Printf("%s parsed, %d/%d points...\n", p.String(), len(tempData.GoLocations), len(data.GoLocations))
+
+		p.Data = tempData
+
+		// log.Printf("Parsed %d points from %s...\n", len(tempData.GoLocations), p.parser.String())
+		return nil
 	}
+
+	numWorkers := runtime.NumCPU() - 2
+
+	dispatcher := pool.NewSingleDispatcher(workerFunc, pool.NewConfig(pool.SetNumConsumers(numWorkers)))
+	dispatcher.Start()
+	defer dispatcher.Close()
+
+	work := []*parserWork{}
+
+	var wg sync.WaitGroup
+
+	for _, p := range parsers {
+		wg.Add(1)
+		pw := &parserWork{
+			parser: p,
+			wg:     &wg,
+		}
+		work = append(work, pw)
+		err := dispatcher.Put(context.Background(), pw)
+		if err != nil {
+			return nil, fmt.Errorf("failed to schedule parser '%s': %w", p.String(), err)
+		}
+	}
+
+	wg.Wait()
+
+	// Collect results from work.
+	for _, pw := range work {
+		if pw.Error != nil {
+			return nil, pw.Error
+		}
+
+		data.GoLocations = append(data.GoLocations, pw.Data.GoLocations...)
+	}
+
+	log.Printf("Collected %d points from %d parsers\n", len(data.GoLocations), len(work))
+
 	return data, nil
 }
