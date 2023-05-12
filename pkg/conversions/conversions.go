94390be0f63a91aa6127653f5c66a2c91c304a96
James Jarvis <git@jamesjarvis.io>
2023-05-12 21:52:46 +0100

    add e7 conversions to conversions package

--- pkg/conversions/conversions.go
+++ pkg/conversions/conversions.go
@@ -1,54 +1,12 @@
+/*
+Package conversions contains useful converter functions used in various parts of mappyboi.
+*/
 package conversions
 
-// import (
-// 	"sort"
+func E7ToWGS84(e7Pos int64) float64 {
+	return float64(e7Pos) / float64(1e7)
+}
 
-// 	"log"
-
-// 	"github.com/jamesjarvis/mappyboi/pkg/models"
-// 	"github.com/tkrajina/gpxgo/gpx"
-// )
-
-// func GoLocationToGPXPoint(goLocation *models.GoLocation) gpx.GPXPoint {
-// 	return gpx.GPXPoint{
-// 		Timestamp:          goLocation.Time,
-// 		HorizontalDilution: *gpx.NewNullableFloat64(goLocation.Accuracy),
-// 		VerticalDilution:   *gpx.NewNullableFloat64(goLocation.VerticalAccuracy),
-// 		Point: gpx.Point{
-// 			Latitude:  goLocation.Latitude,
-// 			Longitude: goLocation.Longitude,
-// 			Elevation: *gpx.NewNullableFloat64(goLocation.Altitude),
-// 		},
-// 	}
-// }
-
-// func ReducePoints(data *models.Data, minDistance float64) (*models.Data, error) {
-// 	log.Printf("Starting point reduction, %d points\n", len(data.GoLocations))
-
-// 	points := make([]gpx.GPXPoint, 0, len(data.GoLocations))
-// 	for _, p := range data.GoLocations {
-// 		points = append(points, GoLocationToGPXPoint(p))
-// 	}
-
-// 	sort.Slice(points, func(i, j int) bool {
-// 		return points[i].Timestamp.Before(points[j].Timestamp)
-// 	})
-
-// 	gpxTrack := &gpx.GPXTrackSegment{
-// 		Points: points,
-// 	}
-// 	gpxTrack.ReduceTrackPoints(minDistance)
-
-// 	log.Printf("Reduced minimum distance between points to %.1fm, %d points (%.0f%% reduction)\n", minDistance, len(gpxTrack.Points), (1-(float64(len(gpxTrack.Points))/float64(len(data.GoLocations))))*100)
-
-// 	newData := &models.Data{
-// 		GoLocations: make([]*models.GoLocation, 0, len(gpxTrack.Points)),
-// 	}
-// 	for _, p := range gpxTrack.Points {
-// 		newData.GoLocations = append(newData.GoLocations, GPXPointToGoLocation(&p))
-// 	}
-// 	data = nil
-// 	data = newData
-
-// 	return data, nil
-// }
+func WGS84ToE7(wgs84pos float64) int64 {
+	return int64(wgs84pos * 1e7)
+}
