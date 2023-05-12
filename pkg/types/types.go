9194b6ac1126fd39a5aa68a2c73b687414bdf736
James Jarvis <git@jamesjarvis.io>
2023-05-12 23:28:39 +0100

    truncate time to avoid any nasty surprises when adding data

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -37,39 +37,26 @@ func cleanupLocation(loc Location) Location {
 	}
 }
 
-// locationKey exists only to serve as a map key.
-type locationKey struct {
-	time      time.Time
-	latitude  float64
-	longitude float64
-}
-
 // LocationHistory stores a structured set of location history.
 type LocationHistory struct {
 	Data []Location // Ordered by Time ASC.
-	seen map[locationKey]struct{}
+	seen map[time.Time]struct{}
 }
 
 // Insert modifies the receiver LocationHistory object by combining it
-// with the incoming Location objects. If the item already exists
+// with the incoming Location objects. If at item with the same time already exists
 // within the map, it will be skipped. To maintain chronological ordering,
 // one must call .Cleanup() afterwards.
 func (lh *LocationHistory) Insert(data ...Location) {
 	if lh.seen == nil {
-		lh.seen = map[locationKey]struct{}{}
+		lh.seen = map[time.Time]struct{}{}
 	}
-	// TODO(jamesjarvis): perf on this sucks, do better.
 	for _, v := range data {
 		cleanValue := cleanupLocation(v)
-		key := locationKey{
-			time:      cleanValue.Time,
-			latitude:  cleanValue.Latitude,
-			longitude: cleanValue.Longitude,
-		}
-		if _, exists := lh.seen[key]; exists {
+		if _, exists := lh.seen[cleanValue.Time]; exists {
 			continue
 		}
-		lh.seen[key] = struct{}{}
+		lh.seen[cleanValue.Time] = struct{}{}
 		lh.Data = append(lh.Data, cleanValue)
 	}
 }
